bf012000a26a9e578beda59d790b0360869fdbef
Christian Meyer <christian.meyer@tum.de>
2021-03-04 21:50:21 +0100

    Added continuous integration (travis) for linux

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
-import setuptools
-setuptools.setup()
+from os.path import join, dirname, abspath
+from setuptools.config import read_configuration
+from setuptools import setup
+
+
+def write_version_py(version, filename=join('src', 'ampardiso', 'version.py')):
+    text = """
+# THIS FILE IS GENERATED FROM AMPARDISO SETUP.PY
+version = '{}'
+""".format(version)
+
+    with open(filename, 'w') as fp:
+        fp.write(text)
+
+
+if __name__ == '__main__':
+    here = abspath(dirname(__file__))
+    metadata = read_configuration(join(here, 'setup.cfg'))['metadata']
+    write_version_py(metadata['version'])
+    setup()
---
+++ tests/test_version.py
@@ -0,0 +1,3 @@
+def test_version():
+    from ampardiso import __version__
+    assert __version__ == '0.0.1'
